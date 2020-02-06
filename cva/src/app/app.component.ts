114488d29c9f61ea8f555a4510543e13464dba51
unknown <j.cserko@gmail.com>
2020-02-06 22:18:29 +0100

    Feat: custom form control.

---
+++ cva/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'cva';
+
+  product = {
+    name: 'Iron',
+    rating: 2
+  };
+}
